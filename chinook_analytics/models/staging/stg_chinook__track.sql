8786db3ccd9dbcd3ea309dc759914cc879a82226
Kevin Manoti <kevinnoty21@gmail.com>
2026-02-16 23:53:21 +0300

    build staging layer: stg_chinook_track

---
+++ chinook_analytics/models/staging/stg_chinook__track.sql
@@ -0,0 +1,24 @@
+with source as (
+
+    select * from {{ source('chinook', 'track') }}
+
+),
+
+renamed as (
+
+    select
+        trackid as track_id,
+        name as track_name,
+        albumid as album_id,
+        mediatypeid as media_type_id,
+        genreid as genre_id,
+        composer as composer,
+        milliseconds as milliseconds,
+        bytes as bytes,
+        unitprice as unit_price
+
+    from source
+
+)
+
+select * from renamed
